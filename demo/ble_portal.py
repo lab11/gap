7f473e973f3743e7a27312ef591717594121dce3
Brad Campbell <bradjc5@gmail.com>
2014-10-29 12:24:16 -0700

    updated

--- demo/ble_portal.py
+++ demo/ble_portal.py
@@ -29,35 +29,40 @@ def websocket_handler (websocket, path):
         queues.append(my_queue)
 
         while True:
-            d = yield from my_queue.get()
+            try:
+                d = yield from my_queue.get()
 
-            # Parse the data from nRF51822
-            fields = struct.unpack('BBB6BbB', d[0:11])
-            bcp_len  = fields[0]
-            bcp_type = fields[1]
-            adv_mac_type = fields[2]
-            adv_mac  = '{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}'.format(*reversed(fields[3:9]))
-            rssi = fields[9]
-            misc = fields[10]
-            data = d[11:]
-            name = 'unknown'
+                # Parse the data from nRF51822
+                fields = struct.unpack('BBB6BbB', d[0:11])
+                bcp_len  = fields[0]
+                bcp_type = fields[1]
+                adv_mac_type = fields[2]
+                adv_mac  = '{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}'.format(*reversed(fields[3:9]))
+                rssi = fields[9]
+                misc = fields[10]
+                data = d[11:]
+                name = 'unknown'
 
-            # Look for a name
-            idx = 0
-            while (len(data)-idx) > 2:
-                l = data[idx]
-                t = data[idx+1]
-                if t == 0x09:
-                    # Found a name
-                    name = data[idx+2:idx+1+l].decode('ascii')
-                idx += l + 1
+                # Look for a name
+                idx = 0
+                while (len(data)-idx) > 2:
+                    l = data[idx]
+                    t = data[idx+1]
+                    if t == 0x09:
+                        # Found a name
+                        name = data[idx+2:idx+1+l].decode('ascii')
+                    idx += l + 1
 
-            adv_pkt = {'mac': adv_mac,
-                       'rssi': rssi,
-                       'data': str(binascii.hexlify(data)),
-                       'name': name}
+                adv_pkt = {'mac': adv_mac,
+                           'rssi': rssi,
+                           'data': str(binascii.hexlify(data)),
+                           'name': name}
 
-            yield from websocket.send(json.dumps(adv_pkt))
+                yield from websocket.send(json.dumps(adv_pkt))
+            except struct.error:
+                pass
+            except: 
+                pass
 
     except:
         queues.remove(my_queue)
