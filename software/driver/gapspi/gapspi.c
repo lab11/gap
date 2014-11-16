ab5f57aa9d589216051f31648f52404cccd0fe48
Brad Campbell <bradjc5@gmail.com>
2014-11-16 16:56:10 -0500

    gapspi compiles

--- software/driver/gapspi/gapspi.c
+++ software/driver/gapspi/gapspi.c
@@ -14,61 +14,59 @@
 #include <linux/delay.h>
 #include <linux/poll.h>
 
-#include "ioctl.h"
+#include "debug.h"
 
 #define DRIVER_AUTHOR  "Neal Jackson <nealjack@umich.edu>"
 #define DRIVER_DESC    "A driver for the GAP Beaglebone Black \"Zigbeag\" cape SPI control."
 #define DRIVER_VERSION "0.1"
 
-void gapspi_cs_mux(int id);
+static void gapspi_cs_mux(int id);
+void gapspi_gpio_free(void);
 
+const char gapspi_name[] = "gapspi";
 
 // Device and Pin config
 #define GAPSPI_NUM_DEVICES 3
-#define GAPSPI_NUM_PINS 2
-#define GAPSPI_E0 45
-#define GAPSPI_E1 44
+#define GAPSPI_NUM_DEMUX_CTRL_PINS 2
+
+// These pins control the DEMUX between the single SPI0 CS pin and
+// the CS line for each radio on GAP
+#define GAPSPI_DEMUX_CTRL_PIN0 45
+#define GAPSPI_DEMUX_CTRL_PIN1 44
+// Array of CS enable pins
+static unsigned int GAPSPI_DEMUX_CTRL_PINS[] = {GAPSPI_DEMUX_CTRL_PIN0,
+                                                GAPSPI_DEMUX_CTRL_PIN1};
+
+// Defines the level of debug output
+uint8_t debug_print = DEBUG_PRINT_ERR;
 
 // SPI
 #define SPI_BUS 1
 #define SPI_BUS_CS0 0
 #define SPI_BUS_SPEED 8000000
 
-// Defines the level of debug output
-uint8_t debug_print = DEBUG_PRINT_ERR;
-const char gapspi_name[] = "gapspi";
-
-// SPI
 struct spi_device* gapspi_spi_device;
 
-static u8 spi_command_buffer[128];
-static u8 spi_data_buffer[128];
+// static u8 spi_command_buffer[128];
+// static u8 spi_data_buffer[128];
 
-static struct spi_transfer spi_tsfers[4];
-static struct spi_message spi_msg;
+// static struct spi_transfer spi_tsfers[4];
+// static struct spi_message spi_msg;
 
-static spinlock_t spi_spin_lock;
-static unsigned long spi_spin_lock_flags;
-static bool spi_pending = false;
+// static spinlock_t spi_spin_lock;
+// static unsigned long spi_spin_lock_flags;
+// static bool spi_pending = false;
 
-// Array of CS enable pins
-static unsigned int CS_ENABLE[] = {GAPSPI_E0, GAPSPI_E1};
 
+// Use this function to set the DEMUX
 static void gapspi_cs_mux(int id)
 {
-	int i = 0;
-	if(id == 0){
-		for(i = 0; i < GAP_NUM_DEVICES; ++i){
-			gpio_set_value(CS_ENABLE[i], 0);
-		}
-		return;
-	}
-	for(i = 0; i < CC2520_NUM_DEVICES; ++i){
-		if(id & (1<<i)){
-			gpio_set_value(CS_ENABLE[i], 1);
-		}
-		else{
-			gpio_set_value(CS_ENABLE[i], 0);
+	int i;
+	for (i = 0; i < GAPSPI_NUM_DEMUX_CTRL_PINS; ++i) {
+		if (id & (1<<i)) {
+			gpio_set_value(GAPSPI_DEMUX_CTRL_PINS[i], 1);
+		} else {
+			gpio_set_value(GAPSPI_DEMUX_CTRL_PINS[i], 0);
 		}
 	}
 }
@@ -77,7 +75,7 @@ int gap_spi_async(struct spi_device * spi,
 				  struct spi_message * message,
 				  int dev_id)
 {
-	gpio_spi_mux(dev_id);
+	gapspi_cs_mux(dev_id);
 	return spi_async(spi, message);
 }
 
@@ -87,7 +85,7 @@ int gap_spi_sync(struct spi_device * spi,
 				  struct spi_message * message,
 				  int dev_id)
 {
-	gpio_spi_mux(dev_id);
+	gapspi_cs_mux(dev_id);
 	return spi_sync(spi, message);
 }
 
@@ -100,25 +98,25 @@ EXPORT_SYMBOL(gap_spi_sync);
 static int gapspi_spi_probe(struct spi_device *spi_device)
 {
     ERR(KERN_INFO, "Inserting SPI protocol driver.\n");
-    nrf51822_spi_device = spi_device;
+    gapspi_spi_device = spi_device;
     return 0;
 }
 
 static int gapspi_spi_remove(struct spi_device *spi_device)
 {
     ERR(KERN_INFO, "Removing SPI protocol driver.");
-    nrf51822_spi_device = NULL;
+    gapspi_spi_device = NULL;
     return 0;
 }
 
 // Configure SPI
 static struct spi_driver gapspi_spi_driver = {
 	.driver = {
-		.name = nrf51822_name,
+		.name = gapspi_name,
 		.owner = THIS_MODULE,
 	},
-	.probe = nrf51822_spi_probe,
-	.remove = nrf51822_spi_remove,
+	.probe = gapspi_spi_probe,
+	.remove = gapspi_spi_remove,
 };
 
 /////////////////
@@ -140,8 +138,8 @@ int init_module(void)
 	//
 
 	// Setup the GPIOs
-	for(i = 0; i < GAPSPI_NUM_PINS; ++i){
-		result = gpio_request_one(CS_ENABLE[i], GPIOF_DIR_OUT, NULL);
+	for(i = 0; i < GAPSPI_NUM_DEMUX_CTRL_PINS; ++i){
+		result = gpio_request_one(GAPSPI_DEMUX_CTRL_PINS[i], GPIOF_DIR_OUT, NULL);
 		if (result) goto error;
 	}
 
@@ -153,7 +151,7 @@ int init_module(void)
 	//
 
 	// Init the SPI lock
-	spin_lock_init(&spi_spin_lock);
+	// spin_lock_init(&spi_spin_lock);
 
 	spi_master = spi_busnum_to_master(SPI_BUS);
 	if (!spi_master) {
@@ -202,7 +200,7 @@ int init_module(void)
 
 	spi_device->controller_state = NULL;
 	spi_device->controller_data = NULL;
-	strlcpy(spi_device->modalias, nrf51822_name, SPI_NAME_SIZE);
+	strlcpy(spi_device->modalias, gapspi_name, SPI_NAME_SIZE);
 
 	result = spi_add_device(spi_device);
 	if (result < 0) {
@@ -213,7 +211,7 @@ int init_module(void)
 	put_device(&spi_master->dev);
 
 	// Register SPI
-	result = spi_register_driver(&nrf51822_spi_driver);
+	result = spi_register_driver(&gapspi_spi_driver);
     if (result < 0) {
     	ERR(KERN_INFO, "Could not register SPI driver.\n");
     	goto error;
@@ -237,14 +235,14 @@ void cleanup_module(void)
 		spi_unregister_device(gapspi_spi_device);
 	}
 
-	spi_unregister_driver(&gapspi_spi_device);
+	spi_unregister_driver(&gapspi_spi_driver);
 }
 
 void gapspi_gpio_free()
 {
 	int i;
-	for(i = 0; i < GAPSPI_NUM_PINS; ++i){
-		result = gpio_free(CS_ENABLE[i]);
+	for (i = 0; i < GAPSPI_NUM_DEMUX_CTRL_PINS; ++i) {
+		gpio_free(GAPSPI_DEMUX_CTRL_PINS[i]);
 	}
 }
 
