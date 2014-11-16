37668889dbac3ef51a5e3a4cf86aea62a49d3e83
Brad Campbell <bradjc5@gmail.com>
2014-11-16 18:41:18 -0500

    add test for receiving advs

---
+++ software/driver/nrf51822/tests/get_adv.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include "../ioctl.h"
+#include "../bcp.h"
+#include <unistd.h>
+
+int main(char ** argv, int argc)
+{
+
+	int result = 0;
+	printf("Testing nrf51822 driver...\n");
+	int file_desc;
+	file_desc = open("/dev/nrf51822_1", O_RDWR);
+
+	printf("Setting nRF51822 to send advertisements.\n");
+	struct nrf51822_simple_command sc;
+	sc.command = BCP_COMMAND_SNIFF_ADVERTISEMENTS;
+	ioctl(file_desc, NRF51822_IOCTL_SIMPLE_COMMAND, &sc);
+
+	int i = 0;
+
+	char buf[256];
+	char pbuf[1024];
+	char *buf_ptr = NULL;
+
+	while (true) {
+		result = read(file_desc, buf, 127);
+
+		printf("result %d\n", result);
+		if (result > 0) {
+			buf_ptr = pbuf;
+			for (i = 0; i < result; i++)
+			{
+				buf_ptr += sprintf(buf_ptr, " 0x%02X", buf[i]);
+			}
+			*(buf_ptr) = '\0';
+			printf("read %s\n", pbuf);
+		}
+	}
+
+	close(file_desc);
+}
